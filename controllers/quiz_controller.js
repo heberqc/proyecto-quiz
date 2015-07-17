b0f91a0681ad614e52a53725a9259fa036356651
Heber Quequejana <heberqc@gmail.com>
2015-07-17 12:15:03 -0500

    Mejorar la búsqueda de una palabra
    Cambio de título de la tabla de búsquedas
    Arreglar parámetro de búsqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,18 +12,10 @@ exports.load = function(req, res, next, quizId) {
   ).catch(function(error) { next(error);});
 };
 
-// // GET /quizes
-// exports.index = function(req, res) {
-//   models.Quiz.findAll().then(function(quizes) {
-//     res.render('quizes/index.ejs', { quizes: quizes});
-//   }).catch(function(error) { next(error);});
-// };
-
 // GET /quizes?search=texto
 exports.index = function(req, res) {
-  var texto = req.query.search;
-  texto = '%' + texto + '%';
-  console.log('El texto es ' + texto);
+  var texto = req.query.search || '';
+  texto = (texto === '')? '%' : '%' + texto + '%';
   models.Quiz.findAll({
     where: {
       pregunta: {
@@ -32,7 +24,7 @@ exports.index = function(req, res) {
     }
   })
   .then(function(quizes) {
-    res.render('quizes/index.ejs', { quizes: quizes});
+    res.render('quizes/index.ejs', { quizes: quizes, texto: req.query.search});
   })
   .catch(function(error) { next(error);});
 };
