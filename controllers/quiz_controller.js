418100344028d4ba6793b3574b5f63ab09585f19
Heber Quequejana <heberqc@gmail.com>
2015-07-20 11:54:57 -0500

    Implementar los métodos para edición de quizes

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -86,3 +86,26 @@ exports.create = function(req, res) {
     }
   );
 };
+
+// GET /quizes/:id/edit
+exports.edit = function (req, res) {
+  var quiz = req.quiz;  // autoload de instancia de quiz
+  res.render('quizes/edit', { quiz: quiz, errors: [] });
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.validate().then(
+    function(err) {
+      if (err) {
+        res.render('quizes/edit', { quiz: req.quiz, errors: err.errors });
+      } else {
+        req.quiz  // save: guarda campos pregunta y respuesta en BBDD
+        .save( { fields: ['pregunta', 'respuesta'] } )
+        .then( function() { res.redirect('/quizes'); } );
+      } // redirección HTTP a la lista de preguntas (URL relativo)
+    }
+  );
+};
--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,7 @@ router.get('/author', function(req, res) {
 });
 router.get('/quizes/new',                   quizController.new);
 router.post('/quizes/create',               quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
+router.put('/quizes/:quizId(\\d+)',         quizController.update);
 
 module.exports = router;
