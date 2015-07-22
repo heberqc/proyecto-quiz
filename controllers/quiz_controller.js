eebf3d07c06f8f45a2a051d662ba535f8d09f93e
Heber Quequejana <heberqc@gmail.com>
2015-07-22 00:41:18 -0500

    Implementar funciones para mostrar el tema de cada quiz

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -64,7 +64,7 @@ exports.answer = function(req, res) {
 // GET /quizes/new
 exports.new = function(req, res) {
   var quiz = models.Quiz.build( // crea objeto quiz
-    { pregunta: "", respuesta: "" }
+    { pregunta: "", respuesta: "", tema: "otro" }
   );
   res.render('quizes/new', { quiz: quiz, errors: [] });
 };
@@ -78,7 +78,7 @@ exports.create = function(req, res) {
         res.render('quizes/new', { quiz: quiz, errors: err.errors });
       } else {
         // guardar en BBDD los campos pregunta y respuesta de quiz
-        quiz.save({ fields: ['pregunta', 'respuesta'] })
+        quiz.save({ fields: ['pregunta', 'respuesta', 'tema'] })
         .then(function() {
           res.redirect('/quizes');
         }); // Redirección HTTP (URL relativo) lista de preguntas
@@ -97,13 +97,14 @@ exports.edit = function (req, res) {
 exports.update = function(req, res) {
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
   req.quiz.validate().then(
     function(err) {
       if (err) {
         res.render('quizes/edit', { quiz: req.quiz, errors: err.errors });
       } else {
         req.quiz  // save: guarda campos pregunta y respuesta en BBDD
-        .save( { fields: ['pregunta', 'respuesta'] } )
+        .save( { fields: ['pregunta', 'respuesta', 'tema'] } )
         .then( function() { res.redirect('/quizes'); } );
       } // redirección HTTP a la lista de preguntas (URL relativo)
     }
