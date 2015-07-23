fa52bec4304d992dd1dec56f643b6fea144ba3b0
Heber Quequejana <heberqc@gmail.com>
2015-07-22 19:35:22 -0500

    Crear tabla 'Comment'

--- models/models.js
+++ models/models.js
@@ -26,10 +26,19 @@ var sequelize = new Sequelize(DB_name, user, pwd,
   }
 );
 
-// Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+// Importar definición de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Importar definición de la tabla Comment
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
