ef724a1aabbd09422505da836a282dc7f14b4628
Heber Quequejana <heberqc@gmail.com>
2015-08-01 17:45:13 -0500

    Implementar el autologout
    Crear el MW 'autoLogout' y colocarlo en las peticiones HTTP

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,6 +1,44 @@
+// Devuelve las horas, minutos y segundos actuales en un objeto
+function getTime() {
+  var now = new Date();
+  return {
+    h: now.getHours(),
+    m: now.getMinutes(),
+    s: now.getSeconds()
+  };
+}
+
+// Convierte la hora en un número de la forma HHMMSS
+function num_tiempo(tiempo) {
+  return tiempo.h * 10000 + tiempo.m * 100 + tiempo.s;
+}
+
+// Compara la hora de la última actividad con la hora actual y devuelve
+// false cuando la diferencia de horas es mayor a 2 minutos
+function control_tiempo(ultimaActividad) {
+  var out = true;
+  var tiempo_actual = getTime();
+  if (num_tiempo(tiempo_actual) - num_tiempo(ultimaActividad) > 200) {
+    out = false;
+  }
+  return out;
+}
+
+// MW que cierra la sesión cuando han pasado 2 minutos desde la última actividad
+exports.autoLogout = function(req, res, next) {
+  if (req.session.user) {
+    if (!control_tiempo(req.session.user.lastTime)) {
+      delete req.session.user;
+      req.session.errors = [{"message": 'La sesión ha expirado luego de 2 minutos'}];
+    }
+  }
+  next();
+};
+
 // MW de autorización de accesos HTTP restringidos
 exports.loginRequired = function(req, res, next) {
   if (req.session.user) {
+    req.session.user.lastTime = getTime();
     next();
   } else {
     res.redirect('/login');
@@ -32,9 +70,13 @@ exports.create = function(req, res) {
 
         // Crear req.session.user y guardar campos   id  y  username
         // La sesión se define por la existencia de:    req.session.user
-        req.session.user = {id:user.id, username:user.username};
+        req.session.user = {
+          id:user.id,
+          username:user.username,
+          lastTime: getTime()
+        };
 
-        res.redirect(req.session.redir.toString());// redirección a path anterior a login
+        res.redirect(req.session.redir.toString() || '/');// redirección a path anterior a login
     });
 };
 
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', sessionController.autoLogout, function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
 
@@ -20,22 +20,29 @@ router.post('/login', sessionController.create);  // crear sesión
 router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
-router.get('/quizes',                       quizController.index);
-router.get('/quizes/:quizId(\\d+)',         quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
-router.get('/author', function(req, res) {
-  res.render('author', { errors: [] });
-});
-router.get('/quizes/new', sessionController.loginRequired, quizController.new);
-router.post('/quizes/create', sessionController.loginRequired, quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
-router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
-router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
+router.get('/quizes', sessionController.autoLogout, quizController.index);
+router.get('/quizes/:quizId(\\d+)', sessionController.autoLogout, quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', sessionController.autoLogout, quizController.answer);
+router.get('/quizes/new', sessionController.autoLogout, sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.autoLogout, sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.autoLogout, sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.autoLogout, sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.autoLogout, sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/new', sessionController.autoLogout,
+  commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', sessionController.autoLogout,
+  commentController.create);
 router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
-  sessionController.loginRequired, commentController.publish);
+  sessionController.autoLogout, sessionController.loginRequired,
+  commentController.publish);
+
+// Definición de ruta de créditos
+router.get('/author', sessionController.autoLogout,
+  function(req, res) {
+    res.render('author', { errors: [] });
+  }
+);
 
 module.exports = router;
